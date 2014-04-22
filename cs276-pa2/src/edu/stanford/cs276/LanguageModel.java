912e967482629e9192276919e837de80e5342e18
Jiaji Hu <jiajihu@stanford.edu>
2014-04-22 12:17:05 -0700

    start working on PA2, finished 2.1

--- cs276-pa2/src/edu/stanford/cs276/LanguageModel.java
+++ cs276-pa2/src/edu/stanford/cs276/LanguageModel.java
@@ -8,6 +8,11 @@ import java.io.FileReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+import edu.stanford.cs276.util.Pair;
 
 
 public class LanguageModel implements Serializable {
@@ -18,13 +23,30 @@ public class LanguageModel implements Serializable {
 	 * 
 	 * Your code here ...
 	 */
-	
-	
+	/*********************************************/
+	private static Map<String, Integer> unaryFreq = new HashMap<String, Integer>();
+	private static Map<Pair<String, String>, Integer> binaryFreq = new HashMap<Pair<String,String>, Integer>();
+  private static int unaryCount = 0;
+  /*********************************************/
+  
 	// Do not call constructor directly since this is a Singleton
 	private LanguageModel(String corpusFilePath) throws Exception {
 		constructDictionaries(corpusFilePath);
 	}
 
+  /*********************************************/
+	public double findUnaryProb(String word){
+	  return (unaryFreq.get(word)+0.0)/unaryCount;
+	}
+	
+	public double findBinaryProb(Pair<String,String> words){
+    return (binaryFreq.get(words)+0.0)/unaryFreq.get(words.getFirst());
+  }
+	
+	public double interpolationProb(Pair<String,String> words, double lambda){
+	  return lambda*findUnaryProb(words.getSecond()) + (1.0-lambda)*(findBinaryProb(words));
+	}
+  /*********************************************/
 
 	public void constructDictionaries(String corpusFilePath)
 			throws Exception {
@@ -42,6 +64,27 @@ public class LanguageModel implements Serializable {
 				/*
 				 * Your code here
 				 */
+			  /*********************************************/
+			  String[] tokens = line.trim().split("\\s+");
+			  for (int i = 0; i < tokens.length; i++) {
+			    if (unaryFreq.containsKey(tokens[i])){
+	          unaryFreq.put(tokens[i], unaryFreq.get(tokens[i])+1);
+	        }
+	        else {
+	          unaryFreq.put(tokens[i], 1);  
+	        }
+			    unaryCount++;
+			    if (i>0){
+			      Pair<String,String> seq = new Pair<String,String> (tokens[i-1],tokens[i]);
+			      if (binaryFreq.containsKey(seq)){
+	            binaryFreq.put(seq, binaryFreq.get(seq)+1);
+	          }
+	          else {
+	            binaryFreq.put(seq, 1);  
+	          }
+			    }
+			  }
+			  /*********************************************/
 			}
 			input.close();
 		}
