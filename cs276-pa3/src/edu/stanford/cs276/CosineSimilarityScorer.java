89869e6783ed7433f1617c54a381601a18f94b2b
Xuening Liu <xnliu@stanford.edu>
2014-05-10 15:07:52 -0700

    cosine similarity done.

--- cs276-pa3/src/edu/stanford/cs276/CosineSimilarityScorer.java
+++ cs276-pa3/src/edu/stanford/cs276/CosineSimilarityScorer.java
@@ -28,47 +28,61 @@ public class CosineSimilarityScorer extends AScorer
     private final double BODY_WEIGHT = -1;
     private final double HEADER_WEIGHT = -1;
     private final double ANCHOR_WEIGHT = -1;
-    
+    private final double[] WEIGHTS = {URL_WEIGHT, TITLE_WEIGHT, BODY_WEIGHT, HEADER_WEIGHT, ANCHOR_WEIGHT};
     private final double SMOOTHING_BODY_LENGTH = -1;
     private final boolean subLinear = false;
-    //////////////////////////////////////////
-	
+    
 	public double getNetScore(Map<Field,Map<String, Double>> tfs, Query q, Map<String,Double> tfQuery,Document d)
 	{
+		/******************************************/
 		double score = 0.0;
-		
-		/*
-		 * @//TODO : Your code here
-		 */
-		
+		int count = 0;
+		for(Field field: Field.values()){
+			double weight = WEIGHTS[count];
+			double temp = 0.0;
+			Map<String, Double> tfs_field = tfs.get(field);
+			for(String word: q.queryWords){
+				temp += tfQuery.get(word) * tfs_field.get(word);
+			}
+			score += temp * weight;
+			count++;
+		}
 		return score;
+		/******************************************/
 	}
 
 	
 	public void normalizeTFs(Map<Field,Map<String, Double>> tfs,Document d, Query q)
 	{
-		/*
-		 * @//TODO : Your code here
-		 */
+		/******************************************/
+		double bodyLen = d.body_length;
+		double norm = bodyLen + 500; // normalize factor
+		for(Field field: Field.values()){
+			Map<String, Double> map = tfs.get(field);
+			for(String word: map.keySet()){
+				map.put(word, map.get(word)/(500.0+bodyLen));
+			}
+		}
+		/******************************************/
 	}
 
 	
 	@Override
 	public double getSimScore(Document d, Query q) 
 	{
-		
 		Map<Field,Map<String, Double>> tfs = this.getDocTermFreqs(d,q,subLinear);
-		
 		this.normalizeTFs(tfs, d, q);
-		
 		Map<String,Double> tfQuery = getQueryFreqs(q,subLinear);
-		
-		
         return getNetScore(tfs,q,tfQuery,d);
 	}
-
 	
-	
-	
-	
+	private double dotProduct (Map<String, Double> v1, Map<String, Double> v2){
+		double res = 0.0;
+		for(String word: v1.keySet()){
+			if(v2.containsKey(word)){
+				res += v2.get(word);
+			}
+		}
+		return res;
+	}
 }
